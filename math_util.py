ddd8b9563e98b092dfe798fd281e3aff4de2e33f
aflores78git <aflores.ai.ia.26@gmail.com>
2026-09-10 15:40:13 -0600

    Update math_util.py

--- math_util.py
+++ math_util.py
@@ -1,1 +1,8 @@
 # Utilerías
+def add(x,y):
+    """Adds two numbers and returns the results."""
+    return x + y
+
+def substract(x,y):
+    """Substracts  two numbers and returns the results."""
+    return x - y
